e079ef15e43ba545bef49d09ed4c9ddd1db91114
Ali Maysha <ali.maysha@northsouth.edu>
2023-05-30 20:01:15 +0600

    This is my first commit.

---
+++ app/src/main/java/com/example/gomental/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.gomental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Setting objects
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please fill all the details", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Successful.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
