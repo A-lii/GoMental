8076151dd3ee503ff46bef031e7a2857b9959f12
Ali Maysha <ali.maysha@northsouth.edu>
2023-06-06 20:13:50 +0600

    This is my second commit.

--- app/src/main/java/com/example/gomental/LoginActivity.java
+++ app/src/main/java/com/example/gomental/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.gomental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +37,13 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(getApplicationContext(), "Login Successful.", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
