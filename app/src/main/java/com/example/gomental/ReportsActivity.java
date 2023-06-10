6e742472ef5e1596868106216ada31e9e864eaeb
Sheefayn <sheefayn.aubora@gmail.com>
2023-06-10 23:20:31 +0600

    This is my fifth commit! <3

--- app/src/main/java/com/example/gomental/ReportsActivity.java
+++ app/src/main/java/com/example/gomental/ReportsActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -63,8 +64,8 @@ public class ReportsActivity extends AppCompatActivity {
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ReportsActivity.this, HomeActivity.class));
+            public void onClick(View view) {
+                startActivity(new Intent(ReportsActivity.this,HomeActivity.class));
             }
         });
 
@@ -76,23 +77,23 @@ public class ReportsActivity extends AppCompatActivity {
             item.put( "line3", packages[i][2]);
             item.put( "line4", packages[i][3]);
             item.put( "line5", "Total Cost: " + packages[i][4] + "/-");
-            list.add(item);
+            list.add( item );
         }
 
         sa = new SimpleAdapter(this, list,
                 R.layout.multi_lines,
-                new String[] {"line1", "line2", "line3", "line4", "line5"},
+                new String[] { "line1", "line2", "line3", "line4", "line5" },
                 new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e});
         listView.setAdapter(sa);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent it = new Intent(ReportsActivity.this,ReportsDetailsActivity.class);
-                it.putExtra("text1", packages[i][0]);
-                it.putExtra("text2", package_details[i]);
-                it.putExtra("text3", packages[i][4]);
-                startActivity(it);
+                Intent intt = new Intent(ReportsActivity.this,ReportsDetailsActivity.class);
+                intt.putExtra("text1", packages[i][0]);
+                intt.putExtra("text2", package_details[i]);
+                intt.putExtra("text3", packages[i][4]);
+                startActivity(intt);
             }
         });
     }
--- app/src/main/java/com/example/gomental/ReportsDetailsActivity.java
+++ app/src/main/java/com/example/gomental/ReportsDetailsActivity.java
@@ -26,7 +26,7 @@ public class ReportsDetailsActivity extends AppCompatActivity {
         tvPackageName = findViewById(R.id.textViewReportsPackageName);
         tvTotalCost = findViewById(R.id.textViewReportsTotalCost);
         edDetails = findViewById(R.id.editTextReportsMultiLine);
-        btnAddToCart = findViewById(R.id.buttonReportsAddToCart);
+        btnAddToCart= findViewById(R.id.buttonReportsAddToCart);
         btnBack = findViewById(R.id.buttonReportsBack2);
 
         edDetails.setKeyListener(null);
@@ -34,35 +34,34 @@ public class ReportsDetailsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         tvPackageName.setText(intent.getStringExtra("text1"));
         edDetails.setText(intent.getStringExtra("text2"));
-        tvTotalCost.setText("Total Cost: "+ intent.getStringExtra("text3") + "/-");
+        tvTotalCost.setText("Total Cost: "+intent.getStringExtra("text3")+"/-");
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ReportsDetailsActivity.this, ReportsActivity.class));
+            public void onClick(View view) {
+                startActivity(new Intent(ReportsDetailsActivity.this,ReportsActivity.class));
             }
         });
 
        btnAddToCart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
-                String username = sharedpreferences.getString("username", "").toString();
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                String username = sharedPreferences.getString("username", "").toString();
                 String product = tvPackageName.getText().toString();
                 float price = Float.parseFloat(intent.getStringExtra("text3").toString());
 
-                Database db = new Database(getApplicationContext(), "GoMental", null, 1);
+                Database db = new Database(getApplicationContext(), "goMental", null, 1);
 
                 if(db.checkCart(username, product)==1){
                     Toast.makeText(getApplicationContext(), "Product Already Added", Toast.LENGTH_SHORT).show();
                 } else {
                     db.addCart(username, product, price, "reports");
-                    Toast.makeText(getApplicationContext(), "Record inserted to Cart", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Record Inserted to Cart", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(ReportsDetailsActivity.this, ReportsActivity.class));
+
                 }
             }
         });
-
-
     }
 }
