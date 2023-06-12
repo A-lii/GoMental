3b3d72b601c2491db3b8be2cdb175dff041a9143
Sheefayn <sheefayn.aubora@gmail.com>
2023-06-12 21:17:30 +0600

    This is my LAST commit! :'(

---
+++ app/src/main/java/com/example/gomental/ProfessionalPanelActivity.java
@@ -0,0 +1,14 @@
+package com.example.gomental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessionalPanelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professional_panel);
+    }
+}
