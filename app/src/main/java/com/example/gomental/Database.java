95488dd8e2da801278a47c23b5178f4967eacb9f
Ali Maysha <ali.maysha@northsouth.edu>
2023-06-12 13:18:06 +0600

    This is my db2.0 commit! :D

--- app/src/main/java/com/example/gomental/Database.java
+++ app/src/main/java/com/example/gomental/Database.java
@@ -3,22 +3,28 @@ package com.example.gomental;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Database extends SQLiteOpenHelper {
     public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, "GoMental.db", null, 1);
+        super(context, name, factory, version + 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String qry1 = "create table users (username TEXT primary key, email TEXT, password TEXT)";
+        String qry1 = "create table users (username text, email text, password text)";
         sqLiteDatabase.execSQL(qry1);
 
         String qry2 = "create table cart (username text,product text, price float, otype text)";
@@ -27,18 +33,22 @@ public class Database extends SQLiteOpenHelper {
         String qry3 = "create table orderplace (username text,fullname text, address text, contactno text, pincode int,date text,time text,amount float,otype text)";
         sqLiteDatabase.execSQL(qry3);
 
+        String qry4 = "create table call (username text, date text, time text, duration text)";
+        sqLiteDatabase.execSQL(qry4);
+
+        String qry5 = "create table text (username text, date text, time text)";
+        sqLiteDatabase.execSQL(qry5);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-           db.execSQL("drop TABLE if exists users");
-           db.execSQL("DROP TABLE IF EXISTS cart");
-           db.execSQL("DROP TABLE IF EXISTS orderplace");
+        db.execSQL("drop TABLE if exists users");
+        db.execSQL("DROP TABLE IF EXISTS cart");
+        db.execSQL("DROP TABLE IF EXISTS orderplace");
     }
 
-
-
-        public Boolean register (String username, String email, String password){
+    public Boolean register (String username, String email, String password){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put("username", username);
@@ -52,14 +62,12 @@ public class Database extends SQLiteOpenHelper {
         }
     }
 
-
-
     public int login(String username, String password) {
         int result = 0;
         String str[] = new String[2];
         str[0] = username;
         str[1] = password;
-        SQLiteDatabase db = this.getReadableDatabase();
+        SQLiteDatabase db = getReadableDatabase();
         Cursor c = db.rawQuery("select * from users where username=? and password=?", str);
         if (c.moveToFirst()) {
             result = 1;
@@ -67,6 +75,7 @@ public class Database extends SQLiteOpenHelper {
         return result;
     }
 
+    /*
     public void addCart(String username, String product, float price, String otype) {
         ContentValues cv = new ContentValues();
         cv.put("username", username);
@@ -77,6 +86,22 @@ public class Database extends SQLiteOpenHelper {
         db.insert("cart", null, cv);
         db.close();
     }
+    */
+
+    public void addCart(String username, String product, float price, String otype) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("username", username);
+        values.put("otype", otype);
+        values.put("product", product);
+        values.put("price", price);
+
+        db.insert("cart", null, values);
+        db.close();
+    }
+
+
 
     public int checkCart(String username, String product) {
         int result = 0;
@@ -92,17 +117,8 @@ public class Database extends SQLiteOpenHelper {
         return result;
     }
 
-    public void removeCart(String username, String otype) {
-        String str[] = new String[2];
-        str[0] = username;
-        str[1] = otype;
-        SQLiteDatabase db = getWritableDatabase();
 
-        db.delete("cart", "username=? and otype=?", str);
-        db.close();
-    }
-
-    public ArrayList getCartData(String username, String otype) {
+  /*  public ArrayList getCartData(String username, String otype) {
         ArrayList<String> arr = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
         String str[] = new String[2];
@@ -118,9 +134,49 @@ public class Database extends SQLiteOpenHelper {
         }
         db.close();
         return arr;
+
+
+    } */
+
+    public void removeCart(String username, String otype) {
+        String str[] = new String[2];
+        str[0] = username;
+        str[1] = otype;
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.delete("cart", "username=? and otype=?", str);
+        db.close();
+    }
+
+
+    public ArrayList<String> getCartData(String username, String otype) {
+        ArrayList<String> cartData = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {"product", "price"};
+        String selection = "username=? AND otype=?";
+        String[] selectionArgs = {username, otype};
+
+        Cursor c = db.query("cart", columns, selection, selectionArgs, null, null, null);
+
+        int productIndex = c.getColumnIndex("product");
+        int priceIndex = c.getColumnIndex("price");
+
+        while (c.moveToNext()) {
+            if (productIndex != -1 && priceIndex != -1) {
+                String product = c.getString(productIndex);
+                double price = c.getDouble(priceIndex);
+                String item = product + "$" + price;
+                cartData.add(item);
+            }
+        }
+
+        c.close();
+        return cartData;
     }
 
     public void addOrder(String username, String fullname, String address, String contact, int pincode, String date, String time, float price, String otype){
+
         ContentValues cv = new ContentValues ();
         cv.put ("username", username);
         cv.put ("fullname", fullname);
@@ -151,8 +207,32 @@ public class Database extends SQLiteOpenHelper {
         db.close();
         return arr;
     }
+
+
+
+    public void addCallHistory(String name, String dateTime, String duration) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("date_time", dateTime);
+        values.put("duration", duration);
+
+        db.insert("call_history", null, values);
+        db.close();
+    }
+
+    public void addCall(String username, String date, String time, String duration) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("username", username);
+        values.put("date", date);
+        values.put("time", time);
+        values.put("duration", duration);
+
+        db.insert("call", null, values);
+        db.close();
+    }
+
 }
-
-
-
-
